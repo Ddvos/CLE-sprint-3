4d91b254815989553d41f1bf2f19fe579a54f6ec
Ddvos <0931703@hr.nl>
2017-11-06 21:49:03 +0100

    Comments aangepast

--- RFID_code_V3.ino
+++ RFID_code_V3.ino
@@ -1,35 +1,48 @@
+/*
+
+Deze code heb ik geschreven voor Sprint 3. In deze sprint heb ik met mijn team de Dierendoos gemaakt.
+
+Door de omschrijving hieronder zal de werking van de code een stuk duidelijker worden.
+
+Werking Dierendoos:
+Om De Dierendoos aan te zetten hoef je hem alleen aan te sluiten op een usb poort hij zal dan automatisch opstarten. 
+Onder het Welkom scherm zitten 6 knoppen waar je een dierenplaat overheen kan leggen. Op de dierenplaat zit een RFID tag die de dierendoos kan lezen zo weet hij welke dierenplaat er op ligt.
+Het is de bedoeling om de 2 bij elkaar horende lichaamsdelen van de dieren met elkaar te verbinden.
+Dit doe je door de knoppen boven het plaatje tegelijk in te drukken. Hiervoor krijg je 2 punten die op het scherm verschijnen. 
+Het is niet mogelijk om het zelfde dier meerdere keren te verbinden. Als je de juiste combinaties  hebt gemaakt kan je de plaat er af halen en vervangen voor een andere dierenplaat. 
+De punten blijven behouden tot dat je de usb kabel loshaalt hierdoor woord het spel gereset en staan de punten weer op 0.
+
+
+Meer informatie kan je vinden op: https://stud.hosted.hr.nl/0884930/kleuterspel/
+*/
 
 #include <SPI.h>
 #include <MFRC522.h>  // library voor de RFID
  
-#define SS_PIN 10 // declaratie pinnnen
+#define SS_PIN 10 // declaratie RFID pinnnen
 #define RST_PIN 9
-MFRC522 mfrc522(SS_PIN, RST_PIN);  
+MFRC522 mfrc522(SS_PIN, RST_PIN);  // functie voor de RFID
 
 #include <Wire.h> 
-#include <LiquidCrystal_I2C.h>
+#include <LiquidCrystal_I2C.h>  // library voor het scherm
 
-// Set the LCD address to 0x27 for a 16 chars and 2 line display
-LiquidCrystal_I2C lcd(0x3F, 16, 2);
 
-int i = 0; //punten per juist antwoord
-boolean e1Combo =false; // dit is de eerste combinatie als hij gemaakt is niet meer bij tellen
-boolean e2Combo =false; // dit is de tweede combinatie als hij gemaakt is niet meer bij tellen
-boolean e3Combo =false; // dit is de derde combinatie als hij gemaakt is niet meer bij tellen
+LiquidCrystal_I2C lcd(0x3F, 16, 2); // dit is het adres voor een 16X2 scherm
 
-boolean e4Combo =false; // enzo...
+int i = 0; // i houd de punten bij op het scherm
+
+// dit zijn per dieren kaart de mogelike combinaties
+boolean e1Combo =false; // als een combinatie is gemaakt komt hij op TRUE zodat hij niet nog een keer gemaakt kan worden
+boolean e2Combo =false; 
+boolean e3Combo =false; 
+
+boolean e4Combo =false; 
 boolean e5Combo =false; 
 boolean e6Combo =false; 
-
-boolean e7Combo =false; 
-boolean e8Combo =false; 
-boolean e9Combo =false; 
 
 
 int greenLedPin = 4;  // ledje als de juiste combinatie is gemaakt
 int redLedPin = 3;   // ledje als er geen of een foute combinatie is gemaakt
- 
-int servoMotor = 8; // dit is de pin die naar de 2de Arduino gaat (Arduino 2 laat de servo draaien)
 
 int knop1 = A0; // input knoppen
 int knop2 = A1;
@@ -49,8 +62,6 @@ int val6 = 0;
 
 String dierenKaart1 = "64 2D E5 20";  // nummer van kaarten (RFID UID)
 String dierenKaart2 = "C4 52 F9 20";
-String dierenKaart3 = "64 FA EF 20";
-
 
 ////////////////////////setup/////////////////////////////////
  
@@ -62,30 +73,26 @@ void setup()
   mfrc522.PCD_Init();  
   Serial.println();
 
-  pinMode(greenLedPin,OUTPUT);
+  pinMode(greenLedPin,OUTPUT);// zet de poorten als in of output 
   pinMode(redLedPin,OUTPUT);
   pinMode(knop1,INPUT);
-  pinMode(servoMotor,OUTPUT);
-
  
- beginScherm(); // in deze functie toont hij het beginscherm, het welkoms woord.
+ beginScherm(); // zodra de arduino opstart gaat hij eenmalig naar deze functie waar hij het welkom woord laat zien.
  
-}
+} // einde setup()
 
 ////////////////////loop//////////////////////////////////
 void loop() 
 {
-  // Look for new cards
-  if ( ! mfrc522.PICC_IsNewCardPresent()) // controleerd voor nieuwe kaarten
+  if ( ! mfrc522.PICC_IsNewCardPresent()) // Als er geen kaarten op de RFID scanner liggen voert hij de volgende code uit:
   {
-    Serial.println("No card");    // geen nieuwe kaarten gevonden
+    Serial.println("No card");     
     digitalWrite(greenLedPin,LOW);
     digitalWrite(redLedPin,LOW);
-    digitalWrite(blueLedPin,LOW); 
     return;
   }
 
-  else{
+  else{ // Als er wel een kaart op de RFID scanner ligt voert hij de volgende code uit
 
                  //// met de onderstaande for loop toont hij de UID van de kaart//////////
             Serial.print("UID tag :");
@@ -94,7 +101,7 @@ void loop()
             for (byte i = 0; i < mfrc522.uid.size; i++) 
             {
                Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
-               Serial.print(mfrc522.uid.uidByte[i], HEX);
+               Serial.print(mfrc522.uid.uidByte[i], HEX);  // hij schrijft naar de Serial monitor de UID code van de RFID kaart
                content.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " "));
                content.concat(String(mfrc522.uid.uidByte[i], HEX));
             }
@@ -103,38 +110,23 @@ void loop()
             Serial.println();
             Serial.print("Message : ");
             content.toUpperCase();
-            if (content.substring(1) == dierenKaart1)//kaart 1//
+            if (content.substring(1) == dierenKaart1)// als de RFID dierenkaart1 leest voert hij de volgende code uit:
             {
                 Serial.println("dierenKaart1");
                 lcd.clear(); 
                 lcd.print("Kaart 1");
-                kaart1(); // gaat naar de functie van de eerste kaart
+                kaart1(); // gaat naar de functie van de eerste dieren kaart
             }
-            else if (content.substring(1) == dierenKaart2)// kaart 2
+            else if (content.substring(1) == dierenKaart2)// als de RFID dierenkaart2 leest voert hij de volgende code uit:
             {
               Serial.println("dierenKaart2");
               lcd.clear(); 
               lcd.print("Kaart 2"); 
-              kaart2(); // gaat naar de functie van de tweede kaart  
-            }
-             else if (content.substring(1) == dierenKaart3)// kaart 3
-            {
-              Serial.println("dierenKaart3");
-              lcd.clear(); 
-              lcd.print("Kaart 3");
-              kaart3(); // gaat naar de functie van de derde kaart    
-            }  
+              kaart2(); // gaat naar de functie van de tweede dieren kaart  
+            } 
     }
-
-    // Select one of the cards
-  if ( ! mfrc522.PICC_ReadCardSerial()) // hier leest hij de kaart uit
-  {
-    return;
-  }
-
     
-    
-} // einde loop 
+} // einde loop() 
 
 
 /*/////////////////////////////////////////////////////////////
@@ -146,7 +138,7 @@ int beginScherm(){  // dit is het welkom scherm
   lcd.backlight();
   lcd.print("     Welkom");
   delay(2000);
-}
+} // einde functie beginScherm()
 
 
 /*/////////////////////////////////////////////////////////////
@@ -155,8 +147,8 @@ int beginScherm(){  // dit is het welkom scherm
 
 int kaart1(){
 
-    val1 = digitalRead(knop1);
-    val2 = digitalRead(knop2);
+    val1 = digitalRead(knop1); //val1 woord nu gelijk gesteld aan knop1
+    val2 = digitalRead(knop2); //enz..
     val3 = digitalRead(knop3);
     val4 = digitalRead(knop4);
     val5 = digitalRead(knop5);
@@ -165,47 +157,43 @@ int kaart1(){
       
     if(val1 == HIGH && val2 == HIGH){ // dit zijn de combinaties die je moet maken om punten te krijgen 
       
-       digitalWrite(greenLedPin,HIGH);
-       digitalWrite(servoMotor,HIGH);
+       digitalWrite(greenLedPin,HIGH); // de juiste combinatie is gemaakt 
        digitalWrite(redLedPin,LOW);
 
-       if(e1Combo ==false){ // als de eerste combinatie is gemaakt telt hij een punt op. en zorgt dat je niet nog een keer de zelfde combinatie kan maken
-       i=i+2;
-       e1Combo = true;
-       }
+       if(e1Combo ==false){ // als de eerste combinatie is gemaakt telt hij 2 punten op. en zorgt dat je niet nog een keer de zelfde combinatie kan maken
+          i=i+2;
+          e1Combo = true; // door e1Combo op true te zetten kunnen de punten niet nog een keer worden opgeteld
+          }
     }
     else if (val3 == HIGH && val4 == HIGH){ //2de combinatie
        digitalWrite(greenLedPin,HIGH);
-       digitalWrite(servoMotor,HIGH);
        digitalWrite(redLedPin,LOW);
 
        if(e2Combo ==false){
-       i=i+2;
-       e2Combo = true;
-       }
+          i=i+2;
+          e2Combo = true;
+          }
       }
 
     else if (val5 == HIGH && val6 == HIGH) { //3de combinatie
        digitalWrite(greenLedPin,HIGH);
-       digitalWrite(servoMotor,HIGH);
        digitalWrite(redLedPin,LOW);
 
-         if(e3Combo ==false){ // 
-       i=i+2;
-       e3Combo = true;
-       }
+         if(e3Combo ==false){  
+            i=i+2;
+            e3Combo = true;
+            }
       }
     
-    else{digitalWrite(redLedPin,HIGH); // nu wordt er geen juist antwoord gegeven
+    else{digitalWrite(redLedPin,HIGH); // nu wordt er geen juiste combinatie gemaakt gegeven
          digitalWrite(greenLedPin,LOW);
-         digitalWrite(servoMotor,LOW);
         
     } 
        lcd.setCursor(0,1); // met deze code toont hij de totale score op het scherm
        lcd.print("score ");
-       lcd.print(i);
-       
-}
+       lcd.print(i); // i is de totale score
+      
+}  // einde functie kaart1()
 
 /*/////////////////////////////////////////////////////////////
                         kaart 2  
@@ -213,8 +201,8 @@ int kaart1(){
 
 int kaart2(){
 
-    val1 = digitalRead(knop1);
-    val2 = digitalRead(knop2);
+    val1 = digitalRead(knop1); //val1 woord nu gelijk gesteld aan knop1
+    val2 = digitalRead(knop2); //enz..
     val3 = digitalRead(knop3);
     val4 = digitalRead(knop4);
     val5 = digitalRead(knop5);
@@ -223,56 +211,44 @@ int kaart2(){
       
       if(val1 == HIGH && val3 == HIGH){ // dit zijn de combinaties die je moet maken om punten te krijgen 
       
-       digitalWrite(greenLedPin,HIGH);
-       digitalWrite(servoMotor,HIGH);
+       digitalWrite(greenLedPin,HIGH); // de juiste combinatie is gemaakt 
        digitalWrite(redLedPin,LOW);
 
        if(e4Combo ==false){ // als de eerste combinatie is gemaakt telt hij een punt op. en zorgt dat je niet nog een keer de zelfde combinatie kan maken
-       i=i+2;
-       e4Combo = true;
+          i=i+2;
+          e4Combo = true; // door e4Combo op true te zetten kunnen de punten niet nog een keer worden opgeteld
        }
     }
    else if (val6 == HIGH && val4 == HIGH){ //2de combinatie
        digitalWrite(greenLedPin,HIGH);
-       digitalWrite(servoMotor,HIGH);
        digitalWrite(redLedPin,LOW);
 
        if(e5Combo ==false){
-       i=i+2;
-       e5Combo = true;
+          i=i+2;
+          e5Combo = true;
        }
       }
 
    else if (val5 == HIGH && val2 == HIGH) { //3de combinatie
        digitalWrite(greenLedPin,HIGH);
-       digitalWrite(servoMotor,HIGH);
        digitalWrite(redLedPin,LOW);
 
-         if(e6Combo ==false){ // 
-       i=i+2;
-       e6Combo = true;
-       }
+         if(e6Combo ==false){   
+            i=i+2;
+            e6Combo = true;
+          }
       }
     
-    else{digitalWrite(redLedPin,HIGH);  // nu wordt er geen juist antwoord gegeven
-         digitalWrite(servoMotor,LOW);
+    else{
+         digitalWrite(redLedPin,HIGH);  // nu wordt er geen juiste combinatie gemaakt
          digitalWrite(greenLedPin,LOW);
-        
-    } 
-       lcd.setCursor(0,1); // met deze code toont hij de totale score op het scherm
-       lcd.print("score ");
-       lcd.print(i);
+        }
+         
+    lcd.setCursor(0,1); // met deze code toont hij de totale score op het scherm
+    lcd.print("score ");
+    lcd.print(i);   // i is de totale score
 
+}  // einde functie kaart2()
 
-}
-
-/*/////////////////////////////////////////////////////////////
-                        kaart 3  
-////////////////////////////////////////////////////////////*/
-
-int kaart3(){
-
-
-}
-
+// einde code
 
